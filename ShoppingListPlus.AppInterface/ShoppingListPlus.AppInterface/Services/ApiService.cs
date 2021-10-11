2a4aab44ab9b9799fec1d486287ab56f011036d1
Bridger Winter <bridgerwinter@gmail.com>
2021-10-10 21:40:10 -1000

    Add project files.

---
+++ ShoppingListPlus.AppInterface/ShoppingListPlus.AppInterface/Services/ApiService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedListPlus.Library.Models;
+using Newtonsoft.Json;
+using ShoppingListPlus.AppInterface.ViewModels;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using System.Net.Http;
+
+namespace ShoppingListPlus.AppInterface.Services
+{
+	class ApiService
+	{
+		public static string RestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "https://bridger.cloud:5001/api" : "https://bridger.cloud:5001/api";
+		private static HttpClient client = new HttpClient();
+		public static HttpClient GetClient()
+		{
+			return client;
+		}
+
+		//Groups
+		public static async Task<Group> GetGroups(long id)
+		{
+			string url = RestUrl + "Groups/" + id;
+			try
+			{
+				string result = await GetClient().GetStringAsync(url);
+				Group group = JsonConvert.DeserializeObject<Group>(result);
+				return group;
+			}
+			catch (Exception)
+			{
+				Group group = new Group();
+				return group;
+				throw;
+			}
+		}
+		public static async Task<HttpResponseMessage> PostGroup(Group group)
+		{
+			string url = RestUrl + "Groups/";
+			// Serialize our concrete class into a JSON String
+			string jsonChore = JsonConvert.SerializeObject(group);
+			// Wrap our JSON inside a StringContent which then can be used by the HttpClient class
+			StringContent httpContent = new StringContent(jsonChore, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = await GetClient().PostAsync(url, httpContent);
+			return message;
+		}
+		public static async Task<HttpResponseMessage> PostFamilyShoppingList(Group group)
+		{
+			string url = RestUrl + "Groups/" + group.GroupId+ "/listitems/create/";
+			string jsonChore = JsonConvert.SerializeObject(group.ListItems[group.ListItems.Count - 1]);
+			StringContent httpContent = new StringContent(jsonChore, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = await GetClient().PostAsync(url, httpContent);
+			return message;
+		}
+		public static async Task<HttpResponseMessage> UpdateGroup(Group group)
+		{
+			string url = RestUrl + "Groups/" + group.GroupId;
+			//serial
+			string jsonChore = JsonConvert.SerializeObject(group);
+			StringContent httpContent = new StringContent(jsonChore, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = await GetClient().PutAsync(url, httpContent);
+			return message;
+		}
+
+		//People
+		public static async Task<HttpResponseMessage> UpdatePerson(Person person)
+		{
+			string url = RestUrl + "People/" + person.PersonId;
+			//serialize bitch
+			string jsonChore = JsonConvert.SerializeObject(person);
+			//wrap our json
+			StringContent httpContent = new StringContent(jsonChore, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = await GetClient().PutAsync(url, httpContent);
+			return message;
+
+		}
+		public static async Task<HttpResponseMessage> PostPerson(Person person)
+		{
+			string url = RestUrl + "People/";
+			// Serialize our concrete class into a JSON String
+			string jsonChore = JsonConvert.SerializeObject(person);
+			// Wrap our JSON inside a StringContent which then can be used by the HttpClient class
+			StringContent httpContent = new StringContent(jsonChore, Encoding.UTF8, "application/json");
+			HttpResponseMessage message = await GetClient().PostAsync(url, httpContent);
+			return message;
+		}
+		public static async Task<Person> GetPerson(long id)
+		{
+			string url = RestUrl + "People/" + id;
+			try
+			{
+				string result = await GetClient().GetStringAsync(url);
+				Person person = JsonConvert.DeserializeObject<Person>(result);
+				return person;
+
+			}
+			catch (Exception)
+			{
+				Person person = new Person();
+				return person;
+				throw;
+			}
+		}
+
+		//ListItems
+		public static async Task<HttpResponseMessage> DeleteShoppingItem(long id)
+		{
+			string url = RestUrl + "ListItems/" + id;
+			HttpResponseMessage message = await GetClient().DeleteAsync(url);
+			return message;
+		}
+	}
+}
---
+++ ShoppingListPlus.AppInterface/ShoppingListPlus.AppInterface/ViewModels/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ShoppingListPlus.AppInterface.ViewModels;
+using SharedListPlus.Library.Models;
+using System.Windows.Input;
+
+namespace ShoppingListPlus.AppInterface.ViewModels
+{
+	public partial class MainPage : ContentPage
+	{
+		public List<ListItem> listOfItems = new List<ListItem>();
+		public MainPage()
+		{
+			InitializeComponent();
+		
+		}
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			ListItem item1 = new ListItem("Eggs");
+			ListItem item2 = new ListItem("Bread");
+			ListItem item3 = new ListItem("Cheese");
+			ListItem item4 = new ListItem("Milk");
+			ListItem item5 = new ListItem("Soap");
+			ListItem item6 = new ListItem("Toilet Paper");
+
+			listOfItems.Add(item1);
+			listOfItems.Add(item2);
+			listOfItems.Add(item3);
+			listOfItems.Add(item4);
+			listOfItems.Add(item5);
+			listOfItems.Add(item6);
+
+			listOfItems.OrderBy(X => X.Item);
+			uiList.ItemsSource = listOfItems;
+		}
+		private void uiList_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			ListItem current = uiList.SelectedItem as ListItem;
+			int index = listOfItems.IndexOf(current);
+			if (current.Item.Contains($"{current.Item[0]}\u0336"))
+			{
+				string swap = current.StrikethroughItem;
+				current.StrikethroughItem = current.Item;
+				current.Item = swap;
+			}
+			else
+			{
+				string swap = current.Item;
+				current.Item = current.StrikethroughItem;
+				current.StrikethroughItem = swap;
+
+			}
+			uiList.ItemsSource = listOfItems.Where(x => x.Item != null).OrderBy(x => x.Item);
+		}
+
+		private void toolbarAdd_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		private void toolbarEdit_Clicked(object sender, EventArgs e)
+		{
+			/*
+			if (uiList.SelectionMode == SelectionMode.Multiple)
+			{
+				this.uiList.SelectionMode = SelectionMode.Multiple;
+				uiList.MultiSe = null;
+				uiList.BackgroundColor = Color.Default;
+				labelIsEditing.IsVisible = false;
+			}
+			else
+			{
+				uiList.MultiSelect = true;
+				uiList.SelectedItem = null;
+				labelIsEditing.IsVisible = true;
+				uiList.BackgroundColor = Color.LightGray;
+			}
+			*/
+		}
+		public void OnEnterPressed(object sender, EventArgs e)
+		{
+			ListItem item = new ListItem(entryNewItem.Text);
+			listOfItems.Add(item);
+			entryNewItem.Text = null;
+			uiList.ItemsSource = listOfItems.Where(x => x.Item != null);
+			//add to the list;	
+		}
+		private void toolbarGroup_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+		private void buttonSortAlpha_Clicked(object sender, EventArgs e)
+		{
+			uiList.ItemsSource = listOfItems.Where(x => x.Item != null).OrderBy(x => x.Item);
+			listOfItems.OrderBy(x => x.Item);
+		}
+		
+
+		private void search_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+			uiList.ItemsSource = listOfItems.Where(x => x.Item.ToLower().Contains(search.Text.ToLower()));
+
+		}
+
+	}
+}
